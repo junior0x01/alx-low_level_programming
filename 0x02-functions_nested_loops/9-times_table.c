955c4edd7e9e08b2b6bdf37262257d602545dda8
Galashop <ahmedgalashop@gmail.com>
2022-12-13 18:45:29 -0800

    DONE

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,35 +4,34 @@
  * Description: prints the 9 times table
  * Return: void
  */
-void times_table(void)
+ void times_table(void)
+ {
+ int i;
+ int j;
+ for (i = 0; i <= 9; i++)
 {
-int row, column, product, tens, ones;
-for (row = 0; row <= 9; row++)
-{
-for (column = 0; column <= 9; column++)
-{
-product = row * column;
-tens = product / 10;
-ones = product % 10;
-if (column == 0)
-{
-_putchar('0');
+ for (j = 0; j <= 9; j++)
+ {
+ int prod = j * i;
+ if (j == 0)
+ {
+	_putchar('0');
 }
-else if (product < 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(ones + '0');
-}
-else
-{
-_putchar(',');
-_putchar(' ');
-_putchar(tens + '0');
-_putchar(ones + '0');
-}
-_putchar('\n');
-}
-}
+ else if (prod <= 9)
+	{
+ _putchar(',');
+ _putchar(' ');
+ _putchar(' ');
+ _putchar(prod + '0');
+			}
+ else
+			{
+ _putchar(',');
+ _putchar(' ');
+ _putchar(prod / 10 + '0');
+ _putchar(prod % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
